75eeb48828d2ae492c153e34184b3a2dce7d3b9e
othinAbbey <othincore@gmail.com>
2023-08-11 12:15:54 +0300

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import Header from './components/Header'
+import Header from "./components/header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,22 +1,22 @@
-import "../components/Header.css" 
-
+import "../components/Header.css";
 
 function Header() {
-    return (
-        <>
-    <header>
-    
-            <h1>KANZU POLICE FORCE</h1>
-            <h3><em>Code and Serve</em></h3>
-    
+  return (
+    <>
+      <header>
+        <h1>KANZU POLICE FORCE</h1>
+        <h3>
+          <em>Code Protect and Serve</em>
+        </h3>
+
         <div className="flag">
-            <span className="black"></span>
-            <span className="yellow"></span>
-            <span className="red"></span>
-         </div>
-    </header>
+          <span className="black"></span>
+          <span className="yellow"></span>
+          <span className="red"></span>
+        </div>
+      </header>
     </>
-    )
+  );
 }
 
-export default Header
+export default Header;

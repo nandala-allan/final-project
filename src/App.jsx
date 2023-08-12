e4cfe6e6abf169eac61704e9bafd14ef340c26b6
NdawulaJoe <ndawulajoseph255@gmail.com>
2023-08-11 23:04:20 -0700

    work on the first form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 // import './App.css'
-import FirstPage from "./components/FirstPage";
+
 import Form from "./components/Form";
+
 function App() {
  return(
   <div>
+ 
    <Form/>
   </div>
  )

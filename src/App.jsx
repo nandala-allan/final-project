e9f2e06b7a95d2b784b9139f3896d8cc1a739eeb
nandala-allan <andala63@gmail.com>
2023-08-14 12:06:23 +0300

    add mantine core

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-
-
 // import './App.css'
-
 import Form from "./components/Form";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
- return(
-  <div>
- 
-   <Form/>
-    <Header />
-  </div>
- )
-
-
+  return (
+    <div>
+      <Header />
+      <Form />
+    </div>
+  );
 }
 
 export default App;

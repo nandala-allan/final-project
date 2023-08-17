d94faf5bb1892e6b230767d46f6adcce34c84178
nandala-allan <andala63@gmail.com>
2023-08-17 10:50:57 +0300

    edit footer and home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,34 +17,35 @@ function Home() {
   return (
     <>
       <Header />
+      <div className="home">
+        <h1>Welcome to Kazu Police Force</h1>
+        <div className="cards">
+          <div className="card-info">
+            <h2>New Form</h2>
+            <p>Make a complaint about a crime</p>
+            <button type="submit" onClick={form}>
+              Generate New
+            </button>
+          </div>
 
-      <h1>Welcome to Kazu Police Force</h1>
-      <div className="cards">
-        <div className="card-info">
-          <h2>New Form</h2>
-          <p>Make a complaint about a crime</p>
-          <button type="submit" onClick={form}>
-            Generate New
-          </button>
+          <div className="card-info">
+            <h2>Search Form</h2>
+            <p>Search for a complaint form</p>
+            <button type="submit" onClick={search}>
+              Search
+            </button>
+          </div>
         </div>
-
-        <div className="card-info">
-          <h2>Search Form</h2>
-          <p>Search for a complaint form</p>
-          <button type="submit" onClick={search}>
-            Search
-          </button>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            inventore ullam necessitatibus labore fugiat velit, quaerat nisi
+            optio odit voluptatibus aliquam nemo id rem, blanditiis nam delectus
+            amet. Optio, officiis.
+          </p>
         </div>
       </div>
-      <div className="content">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          inventore ullam necessitatibus labore fugiat velit, quaerat nisi optio
-          odit voluptatibus aliquam nemo id rem, blanditiis nam delectus amet.
-          Optio, officiis.
-        </p>
-        <Footer />
-      </div>
+      <Footer />
     </>
   );
 }

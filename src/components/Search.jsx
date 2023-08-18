1786bc8d46ba482ddb5dcfaccca598ac28d48bad
nandala-allan <andala63@gmail.com>
2023-08-18 10:45:17 +0300

    edit index.css

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { useForm } from "@mantine/form";
 import { Box, TextInput, NumberInput, Button, Group } from "@mantine/core";
 import Header from "./Header";
+import Footer from "./Footer";
 const border = {
   border: "2px solid rgba(0, 0, 0, 0.1)",
   padding: "20px",
@@ -28,31 +28,33 @@ function Search() {
   return (
     <div>
       <Header />
+      <div className="search-form">
+        <div style={border}>
+          <Box maxWidth={340} mx="50px">
+            <h2>Search Form</h2>
+            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+              <TextInput
+                label="NAME"
+                placeholder="Name"
+                error={form.errors.NAME}
+                {...form.getInputProps("NAME")}
+              />
+              <NumberInput
+                mt="sm"
+                label="ID NUMBER"
+                placeholder="ID Number"
+                error={form.errors.IDnumber}
+                {...form.getInputProps("IDnumber")}
+              />
 
-      <div style={border}>
-        <Box maxWidth={340} mx="50px">
-          <h2>Search Form</h2>
-          <form onSubmit={form.onSubmit((values) => console.log(values))}>
-            <TextInput
-              label="NAME"
-              placeholder="Name"
-              error={form.errors.NAME}
-              {...form.getInputProps("NAME")}
-            />
-            <NumberInput
-              mt="sm"
-              label="ID NUMBER"
-              placeholder="ID Number"
-              error={form.errors.IDnumber}
-              {...form.getInputProps("IDnumber")}
-            />
-
-            <Group position="right" mt="md">
-              <Button type="submit">Search</Button>
-            </Group>
-          </form>
-        </Box>
+              <Group position="right" mt="md">
+                <Button type="submit">Search</Button>
+              </Group>
+            </form>
+          </Box>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }

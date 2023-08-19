d8c9a789b7e05488c133b6b3452a91805377f77c
othinAbbey <othincore@gmail.com>
2023-08-19 21:03:54 +0300

    Move get data function into handleButtonClick function to enable data be rendered only once button is clicked

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,44 +1,55 @@
 import { Input, Button, Container, Flex, Space } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
+import { useState } from "react";
 
 const SearchForm = () => {
-    const getData = async () => {
-        try {
-          const response = await axios.get("http://localhost:1337/api/final-projects");
-          // Handle the response data here
-          console.log(response.data);
-        } catch (error) {
-          // Handle errors here
-          console.error("Error fetching data:", error);
-        }
-      };
-    
-      getData();
+  const [inputValue, setInputValue] = useState("");
+  const [responseData, setResponseData] = useState(null)
+  const handleButtonClick = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:1337/api/final-projects"
+      );
+      // Handle the response data here
+      console.log(response.data);
+    } catch (error) {
+      // Handle errors here
+      console.error("Error fetching data:", error);
+    }
+  }
+  // const getData = async () => {
+   
+  // };
 
-      const handleChenges=(e)=>{
-        const inputValue=e.target.value;
-        console.log(inputValue)
-      }
+  // getData();
 
+  const handleChenges = (e) => {
+    const inputValue = e.target.value;
+    setInputValue(inputValue);
+    console.log(inputValue);
+  };
 
   return (
     <>
       <Container mt={120}>
         <Flex direction={{ base: "column", sm: "row" }} gap="sm" align="center">
           <Input
-          onChange={handleChenges}
+            onChange={handleChenges}
             icon={<IconSearch size={18} />}
             placeholder="Search"
             radius="sm"
           />
-       
-      
-          <Button size="xs" radius="xl">
+
+          <Button size="xs" radius="xl" onClick={}>
             Search
           </Button>
         </Flex>
         <Space h="md" />
       </Container>
+
+      <div>
+        <h1>{`${inputValue}'s Data`}</h1>
+      </div>
     </>
   );
 };

eed73a69478bccb7847a6ea1d327bee6e9264273
othinAbbey <othincore@gmail.com>
2023-08-19 22:33:12 +0300

    Render Data to search page

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,10 +1,11 @@
 import { Input, Button, Container, Flex, Space } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
 import { useState } from "react";
+// import axios from "axios";
 
 const SearchForm = () => {
   const [inputValue, setInputValue] = useState("");
-  const [responseData, setResponseData] = useState(null)
+  const [responseData, setResponseData] = useState(null);
   const handleButtonClick = async () => {
     try {
       const response = await axios.get(
@@ -12,13 +13,46 @@ const SearchForm = () => {
       );
       // Handle the response data here
       console.log(response.data);
+      setResponseData(response.data);
+
+      // setFormData({
+      //   caseDetails: response.data.caseDetails,
+      //   complainerAge: response.data.complainerAge,
+      //   complainerEmail: response.data.complainerEmail,
+      //   complainerId: response.data.complainerId,
+      //   complainerName: response.data.complainerName,
+      //   complainerPlaceOfBirth: response.data.complainerPlaceOfBirth,
+      //   complainerResidence: response.data.complainerResidence,
+      //   complainerTelephone: response.data.complainerTelephone,
+      //   createdAt: response.data.createdAt,
+      //   crimeScene: response.data.crimeScene,
+      //   gender: response.data.gender,
+      //   happenedDate: response.data.happenedDate,
+      //   happenedTime: response.data.happenedTime,
+      //   injuries: response.data.injuries,
+      //   medicalAssistance: response.data.medicalAssistance,
+      //   offence: response.data.offence,
+      //   officerName: response.data.officerName,
+      //   others: response.data.others,
+      //   policeStation: response.data.policeStation,
+      //   publishedAt: response.data.publishedAt,
+      //   rank: response.data.rank,
+      //   referenceNumber: response.data.referenceNumber,
+      //   regestredData: response.data.regestredData,
+      //   updatedAt: response.data.updatedAt,
+      //   victimName: response.data.victimName,
+      //   victimResidence: response.data.victimResidence,
+      //   victimTelephone: response.data.victimTelephone,
+      //   witnessName: response.data.witnessName,
+      //   witnessTel: response.data.witnessTel,
+      // });
     } catch (error) {
       // Handle errors here
       console.error("Error fetching data:", error);
     }
-  }
+  };
   // const getData = async () => {
-   
+
   // };
 
   // getData();
@@ -40,16 +74,19 @@ const SearchForm = () => {
             radius="sm"
           />
 
-          <Button size="xs" radius="xl" onClick={}>
+          <Button size="xs" radius="xl" onClick={handleButtonClick}>
             Search
           </Button>
         </Flex>
         <Space h="md" />
       </Container>
 
-      <div>
-        <h1>{`${inputValue}'s Data`}</h1>
-      </div>
+      {responseData && (
+        <div>
+          <h1>{`${inputValue}'s Data`}</h1>
+          <pre>{JSON.stringify(responseData.data[0], null, 2)}</pre>
+        </div>
+      )}
     </>
   );
 };

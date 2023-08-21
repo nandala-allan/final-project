d33693610a1852acd38309a56225e1a2e12f2e9e
nandala-allan <andala63@gmail.com>
2023-08-21 20:27:47 +0300

    remove strapi files

--- Backend/src/api/final-project/services/final-project.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * final-project service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::final-project.final-project');

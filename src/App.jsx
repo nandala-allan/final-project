e31e6e115c79248c905f9ff6c7b9dcd37c5db13c
nandala-allan <andala63@gmail.com>
2023-08-24 09:24:33 +0300

    edit app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,48 @@
 import LoginButton from "./components/LoginButton";
-import LogOutButton from "./components/LogOutbutton";
-import Home from "./components/Home";
+// import LogOutButton from "./components/LogOutbutton";
+// import Home from "./components/Home";
+
 import "./App.css";
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 function App() {
-  const { isAuthenticated } = useAuth0();
+  // const { isAuthenticated } = useAuth0();
 
   return (
-    <main className="column">
-      <div className="btn">
-      <LoginButton />
-      </div>
+    <>
+      <main className="column">
+        <div>
+          <h2 className="font-bold text-3xl text-white">Kanzu Police Force</h2>
+          <p className="text-md text-white text-center">
+            Code Protect and Serve
+          </p>
+        </div>
+        <img id="splash" src="../Kanzu Police.png" />
+        <div className="home-cards">
+          <div className="info">
+            <h2 className="font-bold text-2xl">Vision</h2>
+            <p className="text-lg">
+              A Professional and People Centered Police for a Safe and Secure
+              Society.
+            </p>
+          </div>
+          <div className="info">
+            <h2 className="font-bold text-2xl">Mission</h2>
+            <p className="text-lg">
+              To Secure Life and Property in Partnership with the Public in a
+              Committed and Professional Manner in order to Promote Sustainable
+              Development
+            </p>
+          </div>
+        </div>
+        <div className="btn">
+          <LoginButton />
+        </div>
 
-      {isAuthenticated && (
-     <div> 
-      <Home />
-      </div>
-      )} 
-    </main>
+        {/* {isAuthenticated && ( */}
+        {/* <div><Home /></div> */}
+        {/* // )}  */}
+      </main>
+    </>
   );
 }
 
--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,20 +1,46 @@
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
+
+// export default function LoginButton() {
+//   const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+//   return(
+//       !isAuthenticated && (
+
+//       <button
+//         className="bg-blue-500 px-4 py-3 font-medium uppercase text-sm text-white"
+//         onClick={() => loginWithRedirect()}
+//       >
+//         Sing In
+//       </button>
+//     )
+
+//   );
+// }
+
+// import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginButton() {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  // const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
 
-  return(
-      !isAuthenticated && (
-      
-      <button
-        className="bg-blue-500 px-4 py-3 font-medium uppercase text-sm text-white"
-        onClick={() => loginWithRedirect()}
-      >
-        Sing In
-      </button>
-    )
- 
-     
-  
+  const handleLoginClick = () => {
+    // if (!isAuthenticated) {
+    // loginWithRedirect();
+    // } else {
+    // Redirect to another page after successful login
+    navigate("/Home"); // Replace with the desired path
+  };
+  // };
+
+  return (
+    // !isAuthenticated && (
+    <button
+      className="bg-blue-500 px-4 py-3 font-medium uppercase text-sm text-white"
+      onClick={handleLoginClick}
+    >
+      Sign In
+    </button>
+    // )
   );
 }

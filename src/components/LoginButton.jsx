d2499b13401dec9a215baf0c95b120e68db0a0ba
nandala-allan <andala63@gmail.com>
2023-08-24 09:31:17 +0300

    edit routing

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -17,30 +17,29 @@
 //   );
 // }
 
-// import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 export default function LoginButton() {
-  // const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
   const navigate = useNavigate();
 
   const handleLoginClick = () => {
-    // if (!isAuthenticated) {
-    // loginWithRedirect();
-    // } else {
-    // Redirect to another page after successful login
-    navigate("/Home"); // Replace with the desired path
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    } else {
+      navigate("/Home");
+    }
   };
-  // };
 
   return (
-    // !isAuthenticated && (
-    <button
-      className="bg-blue-500 px-4 py-3 font-medium uppercase text-sm text-white"
-      onClick={handleLoginClick}
-    >
-      Sign In
-    </button>
-    // )
+    !isAuthenticated && (
+      <button
+        className="bg-blue-500 px-4 py-3 font-medium uppercase text-sm text-white"
+        onClick={handleLoginClick}
+      >
+        Sign In
+      </button>
+    )
   );
 }
